c39610c7165732a93c53e8bd2909c96dab515c60
iperez6508 <iperez6508@001LAB153010809.neisd.ad>
2016-10-13 14:04:57 -0500

    initial commit

---
+++ Hello World!/src/HelloWorld.java
@@ -0,0 +1,24 @@
+
+// GraphicsLab01st.java
+// Student starting version of the GraphicsLab01 assignment.
+// Resave this program as GraphicsLab01v80 for the 80 point version.
+// Repeat this process as you progress to the 90 and 100 point versions.
+
+
+import java.awt.*;
+import java.applet.*;
+public class HelloWorld
+{
+
+	public void paint(Graphics g)
+	{
+		// DRAW CUBE
+		g.drawRect(50, 50, 300, 300);
+		
+		// DRAW SPHERE
+		
+		
+		// DRAW TRIANGLE		
+	}
+
+}
